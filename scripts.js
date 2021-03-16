6028d19645913f58f2f70f356c422d40b761b397
Alkaline-21 <79489164+Alkaline-21@users.noreply.github.com>
2021-03-16 02:01:22 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ Image i = icon.getImage();
 //use random function to select a random-image, assoc boolean
 
 //if click=true and boolean==true, set boolean=false and put image on screen
-var elementIsClicked = false; // declare the variable that tracks the state
-function clickHandler(){ // declare a function that updates the state
+var elementIsClicked = false; 
+function randomizer(){ 
   elementIsClicked = true;
 }
