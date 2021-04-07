9f3dcfd67b0797288e2f4131fdf890c4be7be3be
Alkaline-21 <79489164+Alkaline-21@users.noreply.github.com>
2021-04-07 01:47:58 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 public class code {
-
 let mtnClick = false;
-var mtnArray = new Array();
+var mtnArray = [];
 
 mtnArray[0] = new Image();
 mtnArray[0].src = 'Visuals/Mountains1.jpg';
@@ -15,21 +14,23 @@ mtnArray[2].src = 'Visuals/Mountains3.jpg';
 mtnArray[3] = new Image();
 mtnArray[3].src = 'Visuals/Mountains4.jpg';
 
+var clrMountain;
+
 function selector(category) {
 	if (category === 'mountain') {
 		mtnClick = true;
-		makeLandForms();
+		//makeLandForms();
 	}
 	else if (category === 'arrow') {
 		mtnClick = false;
 	}
 }
 		
-const para = document.querySelectorAll('mtn-button');
-para.addEventListener('click', selector('mountain'));
+const uno = document.querySelectorAll('mtn-button');
+uno.addEventListener('click', selector('mountain'));
 
-const para = document.querySelectorAll('arr-button');
-para.addEventListener('click', selector('arrow'));
+const dos = document.querySelectorAll('arr-button');
+dos.addEventListener('click', selector('arrow'));
 
 document.onclick = userClicked;
 
@@ -41,22 +42,23 @@ function userClicked(event) {
 
 		let which = Math.floor(Math.random()*mtnArray.length);
 
-		if (which = 0) {
-			var clrMountain = document.getElementById("clrMountain1");
+		if (which === 0) {
+			clrMountain = document.getElementById("clrMountain1");
 		}
-		else if (which = 1) {
-			var clrMountain = document.getElementById("clrMountain2");
+		else if (which === 1) {
+			clrMountain = document.getElementById("clrMountain2");
 		}
-		else if (which = 2) {
-			var clrMountain = document.getElementById("clrMountain3");
+		else if (which === 2) {
+			clrMountain = document.getElementById("clrMountain3");
 		}
-		else if (which = 3) {
-			var clrMountain = document.getElementById("clrMountain4");
+		else if (which === 3) {
+			clrMountain = document.getElementById("clrMountain4");
 		}
 
 		clrMountain.style.display = '';
 		clrMountain.style.position = 'absolute';
 		clrMountain.style.left = x + 'px';
 		clrMountain.style.top = y + 'px';
-	}
+    }
+}
 }
