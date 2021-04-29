9ba8daba83f62e9b8c5020b26ec2f0bea6c50dd3
Alkaline-21 <79489164+Alkaline-21@users.noreply.github.com>
2021-04-29 02:42:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -148,6 +148,7 @@ window.addEventListener('click', function(e) {
 			var y = getY(canvas, event);
 			img.style.marginLeft = x;
 			img.style.marginTop = y;
+			img.style.position = 'absolute';
 
 			document.body.appendChild(img);
 		}
@@ -163,14 +164,47 @@ window.addEventListener('click', function(e) {
 			var y = getY(canvas, event);
 			img.style.marginLeft = x;
 			img.style.marginTop = y;
+			img.style.position = 'absolute';
 
 			document.body.appendChild(img);
 		}
    }
 });
 
-function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
+dragElement(document.getElementById("img"));
 
-    var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
-    return { width: srcWidth*ratio, height: srcHeight*ratio };
- }
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
