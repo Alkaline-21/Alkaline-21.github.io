4466356a069dea7539be37e214eae151d383f6ee
Alkaline-21 <79489164+Alkaline-21@users.noreply.github.com>
2021-04-29 02:45:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -176,7 +176,6 @@ dragElement(document.getElementById("img"));
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     elmnt.onmousedown = dragMouseDown;
-  }
 
   function dragMouseDown(e) {
     e = e || window.event;
